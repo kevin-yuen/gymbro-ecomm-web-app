54c81eabf587ef04e0798b96afdcb42ce50d8822
kevin-yuen <kevin.yuen11@gmail.com>
2024-04-25 09:29:38 +0800

    theme color change

--- gymbro-frontend/src/components/oncanvas-navbar/NavLinksComponent.jsx
+++ gymbro-frontend/src/components/oncanvas-navbar/NavLinksComponent.jsx
@@ -4,18 +4,18 @@ import { NavLink } from "react-router-dom";
 export default function NavLinksComponent() {
   return (
     <>
-      <ul className="d-flex flex-direction-row custom-color-antiquewhite list-unstyled ms-4 p-2">
+      <ul className="d-flex flex-direction-row custom-color-antiquewhite custom-font-family-montserrat list-unstyled fs-7 fw-bold ms-4 p-2">
         <li className="pe-5">
-          <NavLink to="/allProducts" className="oncanvas-nav-link text-decoration-none fw-bold">ALL PRODUCTS</NavLink>
+          <NavLink to="/allProducts" className="oncanvas-nav-link text-decoration-none">ALL PRODUCTS</NavLink>
         </li>
         <li className="pe-5">
-          <NavLink to="/womenActivewear" className="oncanvas-nav-link text-decoration-none fw-bold">WOMEN ACTIVEWEAR</NavLink>
+          <NavLink to="/womenActivewear" className="oncanvas-nav-link text-decoration-none">WOMEN ACTIVEWEAR</NavLink>
         </li>
         <li className="pe-5">
-          <NavLink to="/menActivewear" className="oncanvas-nav-link text-decoration-none fw-bold">MEN ACTIVEWEAR</NavLink>
+          <NavLink to="/menActivewear" className="oncanvas-nav-link text-decoration-none">MEN ACTIVEWEAR</NavLink>
         </li>
         <li className="pe-5">
-          <NavLink to="/supplements" className="oncanvas-nav-link text-decoration-none fw-bold">SUPPLEMENTS</NavLink>
+          <NavLink to="/supplements" className="oncanvas-nav-link text-decoration-none">SUPPLEMENTS</NavLink>
         </li>
       </ul>
     </>
