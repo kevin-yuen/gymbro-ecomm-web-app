6e27cc8e76327379a16279e1b813f1bfb76edb8c
kevin-yuen <kevin.yuen11@gmail.com>
2024-06-11 15:13:24 +0800

    set access for cors for signInUser api - attempt #4

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,13 @@ require("dotenv").config();
 const app = express();
 
 // for hosting on Vercel
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: ["https://gymbro-ecomm-web-app-frontend.vercel.app"],
-//     methods: ["POST", "GET", "DELETE", "PATCH", "OPTIONS"],
-//     credentials: true
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://gymbro-ecomm-web-app-frontend.vercel.app",
+    methods: ["POST", "GET", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true
+  })
+);
 
 // middleware
 app.use(express.json());
