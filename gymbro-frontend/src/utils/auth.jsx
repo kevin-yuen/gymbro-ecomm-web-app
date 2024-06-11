29e849f45578ad3015f84aa1fccc7c4bc175904b
kevin-yuen <kevin.yuen11@gmail.com>
2024-06-11 12:17:20 +0800

    changed relative url to absolute url for vercel hosting - attemp #2

--- gymbro-frontend/src/utils/auth.jsx
+++ gymbro-frontend/src/utils/auth.jsx
@@ -1,7 +1,7 @@
 export const handleAuthAPI = async (endpoint, method, json) => {
-  console.log("server URL:", `${process.env.SERVER_URL}`);
-  
-  return await fetch(`${process.env.SERVER_URL}` + endpoint, {
+  console.log("https://gymbro-ecomm-web-app-backend.vercel.app" + endpoint);
+
+  return await fetch("https://gymbro-ecomm-web-app-backend.vercel.app" + endpoint, {
     method: method,
     headers: { "Content-Type": "application/json" },
     body: json,
